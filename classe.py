ca3f0ca238e53f47665df84e60ba41a5cce7d3ed
vitor <vitor.camargo.ofc@gmail.com>
2025-04-09 13:04:43 -0300

    :sparkles: feat: base classe

--- classe.py
+++ classe.py
@@ -1,2 +1,21 @@
-class classe:
-    pass
+class Classe:
+    def __init__(self, nome: str, dado_vida: int, nivel=1):
+        self.__nome = nome
+        self.__dado_vida = dado_vida
+        self.__habilidades = dict()
+        self.__nivel = nivel
+
+    @property
+    def dado_vida(self):
+        return self.__dado_vida
+
+    @dado_vida.setter
+    def dado_vida(self, dado: int):
+        self.__dado_vida = dado
+
+    @property
+    def habilidades(self):
+        return self.__habilidades
+
+    def add_hab(self, nome: str, nivel_necessario: int):
+        self.__habilidades[nome] = nivel_necessario    
