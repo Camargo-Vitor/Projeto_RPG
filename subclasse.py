ec8f6fe72426ae2dccbecb91317a1fd26709a1ae
vitor <vitor.camargo.ofc@gmail.com>
2025-04-13 22:13:18 -0300

    :construction: att: add_hab

--- subclasse.py
+++ subclasse.py
@@ -1,4 +1,5 @@
 from classe import Classe
+from habilidade import Habilidade
 
 class Subclasse(Classe):
     def __init__(self, nome: str, nome_sub: str, dado_vida: int, hab_especificas: list = []):
@@ -15,6 +16,10 @@ class Subclasse(Classe):
         if isinstance(nome_sub, str):
             self.__nome = nome.strip().lower() + nome_sub.strip().lower
 
+    def add_hab(self, habilidade: Habilidade):
+        if isinstance(habilidade, Habilidade) and habilidade.origem == 'subclasse':
+            self.__habilidades.append(habilidade)
+            
     @property
     def hab_especificas(self):
         return self.__hab_espeficicas
