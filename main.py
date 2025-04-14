220472aae5f59cff51afb103ed9de131f74b5054
vitor <vitor.camargo.ofc@gmail.com>
2025-04-13 22:14:08 -0300

    :test_tube: tes: testes

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ if __name__ == '__main__':
     lista_todas_magias =[]
 
     # Habilidades de teste
-    habilidade_teste_especie = Habilidade('Correr', 78, 'especie')
-    habilidade_teste_subespecie = Habilidade('visão noturna', 145, 'subespecie')
-    habilidade_teste_classe = Habilidade('passos furtivos', 176, 'classe')
-    habilidade_teste_subclasse = Habilidade('camuflagem', 201, 'subclasse')
+    habilidade_teste_especie = Habilidade('Correr', 1, 78, 'especie')
+    habilidade_teste_subespecie = Habilidade('visão noturna', 1, 145, 'subespecie')
+    habilidade_teste_classe = Habilidade('passos furtivos', 1, 176, 'classe')
+    habilidade_teste_subclasse = Habilidade('camuflagem', 1, 201, 'subclasse')
 
     lista_todas_habilidades.append(habilidade_teste_especie)
     lista_todas_habilidades.append(habilidade_teste_subespecie)
@@ -29,13 +29,13 @@ if __name__ == '__main__':
 
     # Magias de teste
 
-    magia_teste = Magia('Bola de fogo', 1, 56)
+    magia_teste = Magia('Bola de fogo', 1, 91)
     lista_todas_magias.append(magia_teste)
 
     # classes teste
 
     classe_teste = Classe('Ranger', 6)
-    tiro_preciso = Habilidade('Tiro preciso', 56, 'classe')
+    tiro_preciso = Habilidade('Tiro preciso', 1, 56, 'classe')
     classe_teste.add_hab(tiro_preciso)
 
     # subclasse teste
@@ -46,8 +46,14 @@ if __name__ == '__main__':
     sub_especie_teste = Subespecie('Elfo', 'das montanhas', 10, 133, 'entende a lingua dos elfos')
 
     ficha_teste = Ficha('Roger', 'Comum', 'Caça e tem um passarinho chamado Xavier', classe_teste, sub_especie_teste, ['furtividade'])
-    ficha_teste2 = Ficha('Armin', 'Robusto', 'órfão', subclasse_teste, sub_especie_teste, ['intimidacao'])
+    ficha_teste2 = Ficha('Jurandir', 'Robusto', 'pinga!', subclasse_teste, sub_especie_teste, ['intimidacao'])
 
     #### Playground ####
 
+    ficha_teste.add_magia(magia_teste)
+    ficha_teste.add_item_inventario(item_teste)
+    print(ficha_teste)
+    classe_teste.rm_hab(tiro_preciso)
+    ficha_teste.rm_magia(magia_teste)
+    ficha_teste.rm_item_inventario(item_teste)
     print(ficha_teste)
