5274fa87b11f3783302c31631942d989f3c4ba2c
vitor <vitor.camargo.ofc@gmail.com>
2025-05-14 11:56:49 -0300

    :construction: att: alterar item

--- controller/controlador_item.py
+++ controller/controlador_item.py
@@ -7,7 +7,7 @@ class ControladorItem:
         self.__lista_item = []
         self.__tela_item = TelaItem()
         self.__cod = 1
-    
+
     def pega_item_por_nome(self, nome: str):
         for item in self.__lista_item:
             if item.nome == nome:
@@ -18,7 +18,13 @@ class ControladorItem:
         dados_item = self.__tela_item.pegar_dados_item()
         i = self.pega_item_por_nome(dados_item['nome'])
         if i is None:
-            item = Item(self.__cod, dados_item['nome'], dados_item['valor'], dados_item['raridade'], dados_item['pagina'])
+            item = Item(
+                        self.__cod,
+                        dados_item['nome'],
+                        dados_item['valor'],
+                        dados_item['raridade'],
+                        dados_item['pagina']
+                        )
             self.__cod += 1
             self.__lista_item.append(item)
             self.__tela_item.mensagem('Item criado com sucesso!')
@@ -39,7 +45,7 @@ class ControladorItem:
             return False
 
     def listar_itens(self):
-        self.tela_item.mensagem('Id | Nome | Raridade | Pagina | Valor')
+        self.tela_item.mensagem(f'{"id":^4} | {"nome":^16} | {"Raridade":^10} | {"Pag":^5} | {"valor":^9}')
         for item in self.__lista_item:
             self.tela_item.mostra_item(
                 {
@@ -50,11 +56,31 @@ class ControladorItem:
                     'valor': item.valor
                 }
             )
+
+    def alterar_item_por_id(self):
+        self.listar_itens()
+        identificador = int(self.tela_item.selecionar_item_por_id())
+        for item in self.__lista_item:
+            if item.id == identificador:
+                dados_novos = self.tela_item.pegar_dados_item()
+                item.nome = dados_novos['nome']
+                item.raridade = dados_novos['raridade']
+                item.pagina = dados_novos['pagina']
+                item.valor = dados_novos['valor']
+                return True
+            else:
+                return False
+
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+
     def abre_tela(self):
         opcoes = {
             1: self.incluir_item,
             2: self.excluir_item,
-            3: self.listar_itens
+            3: self.listar_itens,
+            4: self.alterar_item_por_id,
+            0: self.retornar
         }
         while True:
             opc = self.__tela_item.mostra_tela()
@@ -64,7 +90,8 @@ class ControladorItem:
     @property
     def tela_item(self):
         return self.__tela_item
-    
+
     @property
     def lista_item(self):
         return self.__lista_item
+ 
