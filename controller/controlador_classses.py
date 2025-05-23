b31aa9d28585b6efa3e10c695b0c066d49394a43
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2025-05-23 03:20:15 -0300

    🚧 att: MVC classes

--- controller/controlador_classses.py
+++ controller/controlador_classses.py
@@ -12,5 +12,53 @@ class ControladorClasses:
         self.__controlador_sistema = controlador_sistema
         self.__tela_classes = TelaClasses()
         self.__dict__classes : dict[int, Classe] = dict()
-        self.__cod_classe = 1
+        self.__cod = 1
+
+    def pega_classe_por_nome(self, nome: str):
+        for classe in self.__dict__classes.values():
+            if classe.nome== nome:
+                return classe
+        return None
+    
+    def incluir_classe(self):
+        dados_classe = self.tela_classes.pegar_dados_classes()
+        c = self.pega_classe_por_nome(dados_classe['nome'])
+        if c is None:
+            for a in range(3):
+                classe = Classe(
+                    dados_classe['nome'],
+                    dados_classe['dado'],
+                    dados_classe['nome sub'][a]
+                )
+            self.__dict__classes[self.__cod] = classe
+            self.__cod += 1
+            self.tela_classes.mensagem('Classe criada com sucesso!')
+        else:
+            self.tela_classes.mensagem('A classe criada ja existe!')
+    
+    def retornar(self):
+        self.__controlador_sistema.abre_tela()
+
+    def abre_tela(self):
+        opcoes = {
+            1: self.incluir_classe,
+        }
+
+        while True:
+            opc = self.__tela_classes.mostra_tela()
+            metodo = opcoes[opc]
+            metodo()
+
+    @property
+    def controlador_sistema(self):
+        return self.__controlador_sistema
+    
+    @property
+    def tela_classes(self):
+        return self.__tela_classes
+    
+    @property
+    def dict_classes(self):
+        return self.__dict__classes
+            
         
--- views/tela_classes.py
+++ views/tela_classes.py
@@ -7,5 +7,33 @@ class TelaClasses(TelaAbstrata):
         print('2. Excluir Classe')
         print('3. Listar Classes')
         print('4. Modificar Classes')
-        print('0. Retornar')
+        print('0. Retornar ')
         return super().mostra_tela(opcoes)
+    
+    def pegar_dados_classes(self):
+        print('===== Dados Classes =====')
+        nome = input('Digite o nome da classe: ').strip().title()
+        dado_vida = input('Digite o dado de vida: ').strip().title()
+        habilidade = []
+        habilidade_sub = []
+        nomes_sub = []
+        for a in range(3):
+            nome_sub = input('Digite o nome da subclasse: ').strip().title()
+            nomes_sub.append(nome_sub)
+
+        return {
+            'nome': nome,
+            'dado': dado_vida,
+            'nome sub': nomes_sub,
+            'habilidades': habilidade,
+            'habilidades sub': habilidade_sub
+        }
+
+    def selecionar_obj_por_cod(self, obj, total_codigos):
+        return super().selecionar_obj_por_cod(obj, total_codigos)
+    
+    def mostra_classes(self, dados_classe: dict):
+        pass
+
+
+
