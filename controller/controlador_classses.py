af73eb5ee7c0de9a3af3156c0e1f949953568363
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2025-05-23 16:07:57 -0300

    🚧 att: Continuação MVC classe

--- controller/controlador_classses.py
+++ controller/controlador_classses.py
@@ -21,7 +21,7 @@ class ControladorClasses:
         return None
     
     def incluir_classe(self):
-        dados_classe = self.tela_classes.pegar_dados_classes()
+        dados_classe = self.__tela_classes.pegar_dados_classes()
         c = self.pega_classe_por_nome(dados_classe['nome'])
         if c is None:
             for a in range(3):
@@ -32,16 +32,48 @@ class ControladorClasses:
                 )
             self.__dict__classes[self.__cod] = classe
             self.__cod += 1
-            self.tela_classes.mensagem('Classe criada com sucesso!')
+            self.__tela_classes.mensagem('Classe criada com sucesso!')
         else:
-            self.tela_classes.mensagem('A classe criada ja existe!')
+            self.__tela_classes.mensagem('A classe criada ja existe!')
     
+    def listar_classes(self):
+        try:
+            self.__tela_classes.mensagem(f"{'Cod':^4} | {'Nome':^10} | {'Dado':^5} | {'Nome sub':^13} | {'Habilidades':^9} | {'Habilidades sub':^13}")
+            for key, classe in self.__dict__classes.items():
+                self.__tela_classes.mostra_classes(
+                    {
+                        'cod': key,
+                        'nome': classe.nome,
+                        'dado': classe.dado_vida,
+                        'nome sub': [clas.nome for clas in classe.subclasse],
+                        'habilidades': [hab.nome for hab in classe.habilidades],
+ 
+                    }
+                )
+        except Exception as e:
+            self.tela_classes.mensagem(f'ERRO INESPERADO Erro ao listar classe: {e}')
+
+    def excluir_classe(self):
+        try:
+            self.listar_classes()
+            cod_validos = list(self.__dict__classes.keys()) + [0]
+            identificador = self.__tela_classes.selecionar_obj_por_cod('classe', cod_validos)
+            if identificador == 0:
+                return False
+            else:
+                del self.__dict__classes[identificador]
+                self.__tela_classes.mensagem('Classe removida!')
+                return True
+        except Exception as e:
+            self.__tela_classes.mensagem(f'[ERRO INESPERADO] Erro ao excluir classe: {e}') 
+            
     def retornar(self):
         self.__controlador_sistema.abre_tela()
 
     def abre_tela(self):
         opcoes = {
             1: self.incluir_classe,
+            0: self.retornar
         }
 
         while True:
--- views/tela_classes.py
+++ views/tela_classes.py
@@ -2,7 +2,7 @@ from views.tela_abstrata import TelaAbstrata
 
 
 class TelaClasses(TelaAbstrata):
-    def mostra_tela(self, opcoes=...):
+    def mostra_tela(self, opcoes=[1, 2, 3, 4, 0]):
         print('1. Adicionar Classe')
         print('2. Excluir Classe')
         print('3. Listar Classes')
@@ -13,12 +13,13 @@ class TelaClasses(TelaAbstrata):
     def pegar_dados_classes(self):
         print('===== Dados Classes =====')
         nome = input('Digite o nome da classe: ').strip().title()
-        dado_vida = input('Digite o dado de vida: ').strip().title()
+        dado_vida = input('Digite o número do dado de vida: ').strip().title()
         habilidade = []
         habilidade_sub = []
         nomes_sub = []
         for a in range(3):
-            nome_sub = input('Digite o nome da subclasse: ').strip().title()
+            #Por padrão todas as classes possuem 3 subclasses
+            nome_sub = input(f'Digite o nome da {a} ª subclasse: ').strip().title()
             nomes_sub.append(nome_sub)
 
         return {
@@ -33,7 +34,12 @@ class TelaClasses(TelaAbstrata):
         return super().selecionar_obj_por_cod(obj, total_codigos)
     
     def mostra_classes(self, dados_classe: dict):
-        pass
+        print(f"{dados_classe['cod']:^4}", end= ' | ')
+        print(f"{dados_classe['nome']:^10}", end = ' | ')
+        print(f"{dados_classe['dado']:^5}", end= ' | ')
+        print(f"{dados_classe['nome sub']:^13}", end = ' | ')
+        print(f"{str(dados_classe['habilidades']):^ 9}", end= ' | ')
+        print(f"{str(dados_classe['habilidades sub']):^13}")
 
 
 
