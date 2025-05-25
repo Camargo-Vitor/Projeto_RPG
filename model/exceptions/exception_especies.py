c8aca9931c506c57834076b4e522a7145dbab666
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2025-05-25 02:19:23 -0300

    :goal_net: att: especies

--- model/exceptions/exception_especies.py
+++ model/exceptions/exception_especies.py
@@ -1,6 +1,6 @@
 class EspecieJahExisteException(Exception):
-    def __init__(self):
-        super().__init__("[ERRO] A espécie inserida já existe.")
+    def __init__(self, nome: str):
+        super().__init__(f"[ERRO] A espécie {nome} já existe.")
 
 class OrigemInvalidaException(Exception):
     def __init__(self):
