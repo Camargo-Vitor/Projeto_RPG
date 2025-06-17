d2f60f4a91c8dee5d5ff40549fcb0609b4350364
Victor Hugo Fernandes Martins <victorhugofernandesmartins@gmail.com>
2025-06-17 00:10:37 -0300

    ✨ feat: implementação base GUI
    tela sistema

--- views/tela_sistema.py
+++ views/tela_sistema.py
@@ -1,15 +1,52 @@
 from views.tela_abstrata import TelaAbstrata
-
+import PySimpleGUI as sg
 
 class TelaSistema(TelaAbstrata):
+    def __init__(self):
+        self.__window = None
+        self.init_components()
+
     def mostra_tela(self, opcoes=[1, 2, 3, 4, 5, 6, 7, 0]):
-        print('===== Sistema =====')
-        print('1. Item')
-        print('2. Magia')
-        print('3. Habilidade')
-        print('4. Espécie')
-        print('5. Classe')
-        print('6. Pessoa')
-        print('7. Ficha')
-        print('0. sair')
+        self.init_components()
+        button, values = self.__window.Read()
+        opcoes = 0
+        if values['1']:
+            opcoes = 1
+        elif values['2']:
+            opcoes = 2
+        elif values['3']:
+            opcoes = 3
+        elif values['4']:
+            opcoes = 4
+        elif values['5']:
+            opcoes = 5
+        elif values['6']:
+            opcoes = 6
+        elif values['7']:
+            opcoes = 7
+        elif values['0'] or button in (None,'Cancelar'):
+            opcoes = 0
+        self.close()
         return super().mostra_tela(opcoes)
+    
+    def close(self):
+        self.__window.Close()
+
+    def init_components(self):
+        #sg.theme_previewer()
+        sg.ChangeLookAndFeel('Black') #procurar corzinha
+        layout = [
+            [sg.Text('Bem vindo ao sistema de gerenciamento de uma aventura de D&D!', font=('Arial', 25))],
+            [sg.Text('Escolha sua opção', font=("Helvica",15))],
+            [sg.Radio('Item',"RD1", key='1')],
+            [sg.Radio('Magia',"RD1", key='2')],
+            [sg.Radio('Habilidade',"RD1", key='3')],
+            [sg.Radio('Espécie',"RD1", key='4')],
+            [sg.Radio('Classe',"RD1", key='5')],
+            [sg.Radio('Pessoa',"RD1", key='6')],
+            [sg.Radio('Ficha',"RD1", key='7')],
+            [sg.Radio('Finalizar sistema',"RD1", key='0')],
+            [sg.Button('Confirmar'), sg.Cancel('Cancelar')]
+        ]
+        self.__window = sg.Window('Sistema de Gerenciamento de aventura de D&D').Layout(layout)
+        
