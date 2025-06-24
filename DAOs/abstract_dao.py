0dee399f6b131979a59562682cf4896989c6983b
vitor <vitor.camargo.ofc@gmail.com>
2025-06-24 01:16:15 -0300

    :sparkles: feat: inicio de implementação de persistencia

---
+++ DAOs/abstract_dao.py
@@ -0,0 +1,57 @@
+import pickle
+from abc import ABC, abstractmethod
+
+class DAO(ABC):
+    @abstractmethod
+    def __init__(self, datasource=''):
+        self.__datasource = datasource
+        self.__cache = {}
+        try:
+            self.__load()
+        except FileNotFoundError:
+            self.__dump()
+
+    def __dump(self):
+        pickle.dump(self.__cache, open(self.__datasource, 'wb'))
+
+    def __load(self):
+        self.__cache = pickle.load(open(self.__datasource,'rb'))
+
+    #esse método precisa chamar o self.__dump()
+    def add(self, obj):
+        try:
+            cod = max([key for key in self.__cache.keys()]) + 1
+        except Exception:
+            cod = 1
+        self.__cache[cod] = obj
+        self.__dump()  #atualiza o arquivo depois de add novo amigo
+
+    def update(self, key, obj):
+        try:
+            if(self.__cache[key] != None):
+                self.__cache[key] = obj #atualiza a entrada
+                self.__dump()  #atualiza o arquivo
+        except KeyError:
+            pass  # implementar aqui o tratamento da exceção
+
+    def get(self, key):
+        try:
+            return self.__cache[key]
+        except KeyError:
+            pass #implementar aqui o tratamento da exceção
+
+    # esse método precisa chamar o self.__dump()
+    def remove(self, key):
+        self.__cache.pop(key)
+        self.__dump() #atualiza o arquivo depois de remover um objeto
+
+    def get_all(self):
+        return self.__cache.values()
+
+    def get_keys(self):
+        if self.__cache:
+            return self.__cache.keys()
+
+    @property
+    def cache(self):
+        return self.__cache
---
+++ DAOs/magia_dao.py
@@ -0,0 +1,23 @@
+from model.magia import Magia
+from DAOs.abstract_dao import DAO
+
+
+class MagiaDao(DAO):
+    def __init__(self, datasource='magia.pkl'):
+        super().__init__(datasource)
+
+    def add(self, magia: Magia):
+        if((magia is not None) and isinstance(magia, Magia)):
+            super().add(magia)
+
+    def update(self, key, magia: Magia):
+        if((magia is not None) and isinstance(magia, Magia)):
+            super().update(key, magia)
+
+    def get(self, key:int):
+        if isinstance(key, int):
+            return super().get(key)
+
+    def remove(self, key:int):
+        if(isinstance(key, int)):
+            return super().remove(key)
